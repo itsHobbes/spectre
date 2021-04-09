77e373e88b55e7db67c58bda3b703d0a62b8686b
Mark <mark@markg.co.uk>
2021-04-09 18:10:50 +0100

    Initial Commit

---
+++ src/test/java/uk/co/markg/spectre/AppTest.java
@@ -0,0 +1,18 @@
+package uk.co.markg.spectre;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    logger.info("test");
+    assertTrue(true);
+  }
+
+}
